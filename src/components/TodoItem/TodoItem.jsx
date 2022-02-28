68b7445bd00de87863734ea2d92732ba8a0d09e4
Andrey Dokuchayev <andrey.d@pcgamingrace.com>
2022-02-28 16:37:54 -0600

    icons: checkmark, delete

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,5 +1,9 @@
 import './TodoItem.css';
 
+import { FaTrash } from 'react-icons/fa';
+import { FaRegCheckSquare } from 'react-icons/fa';
+import { FaRegSquare , FaCheckSquare } from 'react-icons/fa';
+
 
 function TodoItem({ todo }){
 	
@@ -12,8 +16,26 @@ function TodoItem({ todo }){
 			
 			{/* <Checkbox checked={todo.done} checkboxClicked={(checked) => checkboxClicked(checked) } /> */}
 			
-			<p>{todo.title}</p>
-			<p>{"done: " + todo.isDone}</p>
+			<div className='left'>
+				
+				{todo.isDone ? 
+					// <FaRegCheckSquare className='check-box'/> 
+					<FaCheckSquare className='check-box'/> 
+					
+					: 
+					<FaRegSquare className='check-box' /> 
+				}
+				
+				<p className='todo-title'>{todo.title}</p>
+			</div>
+			
+			
+			{/* <p>{"done: " + todo.isDone}</p> */}
+			
+			<FaTrash className='delete-icon'/>
+			
+			
+			
 			
 		</div>
 		
